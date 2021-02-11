fa506957a58b8df5b6769888f8d92e82a6276d33
Frederico Espeschit <espeschit@MacBook-Pro-de-Frederico.local>
2021-02-10 22:36:13 -0300

    Iniciando com JavaScript

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,9 @@
+var nome = 'Frederico';
+console.log(typeof nome) //typeof verifica qual é o tipo de dado de uma variavel (ex: String)
+
+var nome = 'Frederico';
+var sobrenome = 'Espeschit';
+var nomeCompleto = nome + sobrenome
+console.log(nomeCompleto)
+
+
---
+++ variaveis/script.js
@@ -0,0 +1,26 @@
+//console.log('Frederico Espeschit')
+
+//Exercíos
+//Declaras uma variável com o seu nome
+var nome = 'Frederico Espeschit';
+console.log(nome)
+// Declarar uma variável com a sua idade
+var idade = 28;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comida;
+
+// Atribuir valor a sua comida Favorita
+comida = 'Lasanha'
+console.log(comida);
+// Declarar 5 variaveis diferentes sem valores
+
+var nome, 
+    idade, 
+    pais,
+    estado,
+    cidade;
+
+    console.log(nome, idade)
+
+    
