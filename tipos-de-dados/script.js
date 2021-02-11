54c6342e69d757ccea7928059495377008408263
Frederico Espeschit <fespeschitt@gmail.com>
2021-02-11 19:30:07 -0300

    Tipos de dados

--- tipos-de-dados/script.js
+++ tipos-de-dados/script.js
@@ -4,7 +4,36 @@ console.log(typeof nome) //typeof verifica qual é o tipo de dado de uma variave
 var nome = 'Frederico';
 var sobrenome = 'Espeschit';
 var nomeCompleto = nome + sobrenome
-console.log(nomeCompleto)
+console.log(nomeCompleto);
 
 
 console.log('Git na me irrita')
+
+
+
+//Exercícios
+//Declare uma variavél contendo uma string
+var estado = 'Minas Gerais';
+
+//Declare uma variavél contendo um número dentro da uma string
+var numero = '28';
+
+//Declare uma variavél com sua idade
+var idade = 28;
+
+//Declare duas variveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var primeiroNome = 'Frederico',
+    ultimoNome = 'Espeschit',
+    nomeCompleto = primeiroNome + ' ' + ultimoNome
+
+//template string
+var nomeMae = 'Jussara',
+    sobrenomeMae = 'Rodrigues',
+    nomeCompletoMae = `${nomeMae} ${sobrenomeMae}`
+
+//Coloque a seguinte frase em uma variavél: It's time
+var frase = 'It\'s time'
+var texto = `It's my life`
+
+    
+console.log(estado, (typeof numero), idade, nomeCompleto, frase, nomeCompletoMae, texto)
